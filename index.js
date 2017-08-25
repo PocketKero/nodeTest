73b2847d29317b69557087642c45e5baff967fef
Pocket <cpocketc@gmail.com>
2017-08-25 12:07:07 +0900

    CSV(UTF-8)の中身を一部書き換えるようにした

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿const fs = require('fs');
+const rewriteCSV = require('./rewriteCSV.js');
+
+const inputPath = process.argv[2];
+const filename = process.argv[3];
+
+try
+{
+	if(fs.statSync(inputPath))
+	{
+		rewriteCSV(inputPath, filename);
+	}
+}
+catch(e)
+{
+	throw e;
+}
+
---
+++ rewriteCSV.js
@@ -0,0 +1,23 @@
+﻿const fs = require('fs')
+const csvSync = require('csv-parse/lib/sync');
+const iconv = require('iconv-lite');
+
+
+module.exports = function(inputPath, filename)
+{
+	let str = "";
+	let data = fs.readFileSync(inputPath);
+	let res = csvSync(data);
+	if(res[14][0] == '電子ファイル名')
+	{
+		res[14][1] = filename;
+		res.forEach(row => str += row.join(',') + '\n');
+		let buffer = iconv.encode(str, "SJIS");
+		fs.writeFile(inputPath, buffer);
+	}
+	else
+	{
+		throw new Error("参照するレコードが違います");
+	}
+};
+
