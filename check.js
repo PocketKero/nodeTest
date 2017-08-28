ff61ff3b55dd373bf831849d24dac6382dd7d124
Pocket <cpocketc@gmail.com>
2017-08-28 13:22:56 +0900

    package.jsonと.gitignoreを追加

--- check.js
+++ check.js
@@ -18,35 +18,30 @@ module.exports = (uploadDir) => {
 		csvData = csvSync(iconv.decode(buffer, "SJIS"));
 		//console.log('CSVファイルの行数=' + data.length);
 		try {
-			if (fs.statSync(path.resolve(uploadDir, dirName, 'entry_sheet.csv'))) {
-				// console.log(dirName);
-				// console.log("エントリーシート検出");
-				const fileExt = path.extname(csvData[14][1]);
-				const fileName = path.basename(csvData[14][1], fileExt);
-				const ext = csvData[16][1];
-
-				if (fileName == dirName && fs.statSync(path.resolve(uploadDir, dirName, fileName+fileExt)) && fileExt == ext) {
-					// console.log("　　あってます");
-				} else {
-					if (fileName != dirName) {
-						throw new Error("　　ディレクトリ名とエントリーシートに書かれたファイル名が一致しないよ" + "\n　　ディレクトリ名…" + dirName + "\n　　ファイル名…" + fileName);
-					}
-					try {
-						if (!fs.statSync(path.resolve(uploadDir, dirName, fileName+fileExt)));
-					}
-					catch (e) {
-						throw new Error("　　エントリーシートの表記と実際のファイルの拡張子が一致しないよ");
-					}
-					if (fileExt != ext) {
-						throw new Error("　　エントリーシート15行目と17行目の拡張子が一致しないよ" + "\n　　15行目の拡張子…" + fileExt + "\n　　17行目の拡張子…" + ext);
-					}
-				}
-			}
+			fs.statSync(path.resolve(uploadDir, dirName, 'entry_sheet.csv'));
 		}
 		catch (e) {
-			throw e
+			throw e;
 		}
+			
+		// console.log(dirName);
+		// console.log("エントリーシート検出");
+		const fileExt = path.extname(csvData[14][1]);
+		const fileName = path.basename(csvData[14][1], fileExt);
+		const ext = csvData[16][1].split('／');	
 
+		if (fileName != dirName) {
+			throw new Error("　　ディレクトリ名とエントリーシートに書かれたファイル名が一致しないよ" + "\n　　ディレクトリ名…" + dirName + "\n　　ファイル名…" + fileName);
+		}
+		try {
+			if (!fs.statSync(path.resolve(uploadDir, dirName, fileName+fileExt)));
+		}
+		catch (e) {
+			throw new Error("　　エントリーシートの表記と実際のファイルの拡張子が一致しないよ");
+		}
+		if (!ext.some((element) => (element == fileExt))) {
+			throw new Error("　　エントリーシート15行目と17行目の拡張子が一致しないよ" + "\n　　15行目の拡張子…" + fileExt + "\n　　17行目の拡張子…" + ext);
+		}
 	});
 
 }
