109e4cc9752e63fd2a129606a505e2431fb3d0e9
Pocket <cpocketc@gmail.com>
2017-08-28 09:14:18 +0900

    index.jsからcheck.jsを実行するようにした

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 ﻿const fs = require('fs');
-const rewriteCSV = require('./rewriteCSV.js');
+const check = require('./check.js');
 
 const inputPath = process.argv[2];
-const filename = process.argv[3];
 
 try
 {
 	if(fs.statSync(inputPath))
 	{
-		rewriteCSV(inputPath, filename);
+		check(inputPath);
 	}
 }
 catch(e)
