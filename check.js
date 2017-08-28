c493c6b43a5b8db451d739edde94df2cc50ba2c8
Pocket <cpocketc@gmail.com>
2017-08-28 10:31:04 +0900

    繰り返し部分をforEachで行うようにした

--- check.js
+++ check.js
@@ -8,34 +8,31 @@ const path = require('path')
 
 module.exports = (uploadDir) => {
 
-	let nowCount = 0;
-
-	const fileNames = fs.readdirSync(path.resolve(uploadDir)).filter((fileName) => {
-		return fileName !== 'check.js' && fileName !== 'node_modules' && !/^\..+$/.test(fileName)
+	const dirNames = fs.readdirSync(path.resolve(uploadDir)).filter((dirName) => {
+		return dirName !== 'check.js' && dirName !== 'node_modules' && !/^\..+$/.test(dirName)
 	})
 
-	async.forever((callback) => { /* 処理1 */
-		let csvData = fs.readFileSync(path.resolve(uploadDir, fileNames[nowCount], 'entry_sheet.csv'));
+	dirNames.forEach((dirName) => {
+		let csvData = fs.readFileSync(path.resolve(uploadDir, dirName, 'entry_sheet.csv'));
 		let buffer = new Buffer(csvData, 'binary');
 		csvData = csvSync(iconv.decode(buffer, "SJIS"));
 		//console.log('CSVファイルの行数=' + data.length);
 		try {
-			if (fs.statSync(path.resolve(uploadDir, fileNames[nowCount], 'entry_sheet.csv'))) {
-				const __dirname = fileNames[nowCount];
-				// console.log(__dirname);
+			if (fs.statSync(path.resolve(uploadDir, dirName, 'entry_sheet.csv'))) {
+				// console.log(dirName);
 				// console.log("エントリーシート検出");
 				const fileExt = path.extname(csvData[14][1]);
 				const fileName = path.basename(csvData[14][1], fileExt);
 				const ext = csvData[16][1];
 
-				if (fileName == __dirname && fs.statSync(path.resolve(uploadDir, __dirname, fileName+fileExt)) && fileExt == ext) {
+				if (fileName == dirName && fs.statSync(path.resolve(uploadDir, dirName, fileName+fileExt)) && fileExt == ext) {
 					// console.log("　　あってます");
 				} else {
-					if (fileName != __dirname) {
-						throw new Error("　　ディレクトリ名とエントリーシートに書かれたファイル名が一致しないよ" + "\n　　ディレクトリ名…" + __dirname + "\n　　ファイル名…" + fileName);
+					if (fileName != dirName) {
+						throw new Error("　　ディレクトリ名とエントリーシートに書かれたファイル名が一致しないよ" + "\n　　ディレクトリ名…" + dirName + "\n　　ファイル名…" + fileName);
 					}
 					try {
-						if (!fs.statSync(path.resolve(uploadDir, __dirname, fileName+fileExt)));
+						if (!fs.statSync(path.resolve(uploadDir, dirName, fileName+fileExt)));
 					}
 					catch (e) {
 						throw new Error("　　エントリーシートの表記と実際のファイルの拡張子が一致しないよ");
@@ -50,13 +47,6 @@ module.exports = (uploadDir) => {
 			throw e
 		}
 
-		nowCount++
-		// 引数nullのcallbackを呼び出すと、再度「処理1」が呼ばれる
-		if (nowCount != fileNames.length) callback(null);
-		// callbackにnull以外の値を渡すと「処理2」に移り、ループが終了する
-		else callback('end');
-	}, (end) => {
-		/* 処理2 */
 	});
 
 }
