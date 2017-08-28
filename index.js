fa28e0e9db620e243b6ba79ce7967b2403064053
Pocket <cpocketc@gmail.com>
2017-08-28 13:43:27 +0900

    xlsxファイルをCSVファイルに変換する機能を追加

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 ﻿const fs = require('fs');
+const xlsxToCSV = require('./xlsxToCSV.js');
+const rewriteCSV = require('./rewriteCSV.js');
 const check = require('./check.js');
 
 const inputPath = process.argv[2];
+const outputPath = process.argv[3];
 
 try
 {
-	if(fs.statSync(inputPath))
-	{
-		check(inputPath);
-	}
+	fs.statSync(inputPath);
 }
 catch(e)
 {
 	throw e;
 }
 
+xlsxToCSV(inputPath, outputPath);
+//rewirteCSV(inputPath);
+//check(inputPath);
---
+++ xlsxToCSV.js
@@ -0,0 +1,18 @@
+﻿const fs = require('fs');
+const csv = require('fast-csv');
+const jconv = require('jconv');
+const xlsx = require('xlsx');
+const utils = xlsx.util;
+const path = require('path');
+
+module.exports = function(inputPath, outputPath)
+{
+	const workbook = xlsx.readFile(inputPath); // Excelファイルを読み込む
+	const sheetName = workbook.SheetNames[0]; // Excelファイルの最初のシートの名前を取得
+	const worksheet = workbook.Sheets[sheetName]; // Excelファイルの最初のシートを読み込む
+
+	const csv = xlsx.utils.sheet_to_csv(worksheet); // ExcelデータをCSV形式で取得
+
+	const buffer = jconv.convert(csv, 'UTF8', 'SJIS'); // UTF-8からShift-JISへ文字コードを変換
+	fs.writeFileSync(path.resolve(outputPath, 'entry_sheet.csv'), buffer); // ファイルを書き出し
+}
